641835ed5daafeb7e765ca50e8d179abcd90a884
Jason Rodgers <jasonrodgers@Jasons-MacBook-Pro.local>
2021-01-22 12:42:47 -0500

    Make lib.js file as well as change script type to module

---
+++ lib.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+const span = form.querySelector("span");
+
+export default {
+  button: form.querySelector("button"),
+  charCount: span.querySelector("span"),
+  textarea: form.querySelector("textarea"),
+};
