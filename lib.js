29f124cd944f10788e427ce74b4af171ed4d5a3d
Jason Rodgers <jasonrodgers@Jasons-MacBook-Pro.local>
2021-01-22 13:27:28 -0500

    refactor code to beeasier to read

--- lib.js
+++ lib.js
@@ -1,9 +1,25 @@
 const form = document.querySelector("form");
 const span = form.querySelector("span");
 
-export default {
+export const dom = {
   button: form.querySelector("button"),
   charCountSpan: span.querySelector("span"),
   span, // span: span
   textarea: form.querySelector("textarea"),
+};
+
+export const toggleBtn = (count) => {
+  if (count) {
+    dom.button.disabled = false;
+  } else {
+    dom.button.disabled = true;
+  }
+};
+
+export const toggleCharCountError = (count) => {
+  if (count > 140) {
+    dom.span.classList.add("is-error");
+  } else {
+    dom.span.classList.remove("is-error");
+  }
 };
--- scripts.js
+++ scripts.js
@@ -1,17 +1,8 @@
-import dom from "./lib.js";
+import { dom, toggleBtn, toggleCharCountError } from "./lib.js";
 
 dom.textarea.addEventListener("keyup", function handleTextarea() {
   const charCount = this.value.length;
-  if (charCount) {
-    dom.button.disabled = false;
-  } else {
-    dom.button.disabled = true;
-  }
   dom.charCountSpan.innerText = charCount;
-
-  if (charCount > 140) {
-    dom.span.classList.add("is-error");
-  } else {
-    dom.span.classList.remove("is-error");
-  }
+  toggleBtn(charCount);
+  toggleCharCountError(charCount);
 });
